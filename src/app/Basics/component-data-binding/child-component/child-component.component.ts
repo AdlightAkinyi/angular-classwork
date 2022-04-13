b9be568de9f7e8cda3ee26160cfde50ff22f4d75
C0159412020 <adlightakinyi1@gmail.com>
2022-04-13 21:42:16 +0300

    edited

---
+++ src/app/Basics/component-data-binding/child-component/child-component.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit ,Output, EventEmitter  } from '@angular/core';
+
+@Component({
+  selector: 'app-child-component',
+  templateUrl: './child-component.component.html',
+  styleUrls: ['./child-component.component.css']
+})
+export class ChildComponentComponent implements OnInit {
+  @Output() addcity = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    addItem() {
+      this.addcity.emit('Nairobi'); 
+  }
+
+  }
